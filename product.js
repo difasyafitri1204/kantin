a36bb704b8e1eb5649326b0308f4dfa1eac75984
difasyafitri1204 <dfsyr08@gmailcom>
2023-03-28 09:37:55 +0700

    difa

--- product.js
+++ product.js
@@ -1,36 +1,23 @@
-// const dbAdd = {
-//     save(ListAdd2) {
-//         localStorage.setItem('ListAdd', JSON.stringify(ListAdd2));
-//     },
-//     get() {
-//         return JSON.parse(localStorage.getItem('ListAdd'));
-//     }
-// }
-
-// const ListAdd = {
-//     Add: {
-//         index: -1,
-//         names: null,
-//         price: null,
-//         pictures: null
-//     },
-//     showListAdd: function() {
-//         this.ListAdd = dbAdd.get();
-//         const ListProduct = document.getElementById('List-Product');
-//             this.ListAdd.forEach((item) => {
-//             ListProduct.innerHTML += `
-//             <div class="grid grid-flow-col auto-cols-max">
-//             <div class="card w-96 bg-base-100 shadow-xl">
-//                 <p>
-//                 <h2>${item.names}</h2>
-//                 <h3>${item.price} </h3>
-//                 <img src="${item.pictures}" width= "100px">
-//                 </p>
-//             </div>
-//             </div>`
-//         })
-//     }
-// }
-
-// ListAdd.showListAdd();
-
+const dbLogin = {
+    save(IndexLogin2) {
+        localStorage.setItem('IndexLogin', JSON.stringify(IndexLogin2));
+    },
+    get() {
+        return JSON.parse(localStorage.getItem('IndexLogin'));
+    }
+}
+function logout() {
+    IndexLogout.logout();
+}
+const IndexLogout = {
+    beginAgain: function(){ 
+        this.login = dbLogin.get(); 
+        if (!this.Login) {
+            window.location.href="index.html";
+        }
+    },
+    logout: function () {
+        localStorage.removeItem('IndexLogin');
+    }
+}
+IndexLogout.beginAgain()

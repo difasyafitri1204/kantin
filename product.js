e312e19b9b26d2794880b6415ff4c7b3f83d7e11
ynisnty <98867711+Yunisintiya@users.noreply.github.com>
2023-03-29 10:18:04 +0700

    yuni

---
+++ product.js
@@ -0,0 +1,24 @@
+const dbAdd = {
+    save(ListAdd2) {
+        localStorage.setItem('ListAdd', JSON.stringify(ListAdd2));
+    },
+    get() {
+        return JSON.parse(localStorage.getItem('ListAdd'));
+    }
+}
+
+const ListAdd = {
+    showListAdd: function() {
+        this.ListAdd = dbAdd.get();
+        const listOption = document.getElementById('products');
+            this.ListAdd.forEach((item) => {
+            listOption.innerHTML += `
+            <div class="container mx-auto">
+              <div class="flex justify-center gap-5"> 
+                  <div class="card card-compact w-96 bg-base-100 shadow-xl">${item.names}<br><img src="${item.pictures}" 
+                width= "200px" height="200px" ><br>${item.price} </div></div></div>`
+        })
+    }
+}
+
+ListAdd.showListAdd();

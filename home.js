6396c42baacde9fe1c9061373fc36eb81185c977
difasyafitri1204 <89449073+difasyafitri1204@users.noreply.github.com>
2023-03-29 11:30:06 +0700

    update d

--- home.js
+++ home.js
@@ -1,18 +1,18 @@
-const consumer = {
-    showListConsumer: function() {
-        this.ListConsumer = dbConsumer.get();
-        const componentListConsumer = document.getElementById('List-Consumer');
-        componentListConsumer.innerHTML = '';
-        if (this.ListConsumer === null) {
-            console.log ('tidak memiliki data');
-        } else {
-                this.ListConsumer.forEach((Consumer, index) => {
-                componentListConsumer.innerHTML +=   `<h4><div class="flex justify-center gap-5">
-                <div class="grid grid-flow-col auto-cols-max">${Consumer.names} <br> ${Consumer.class}  <br> ${Consumer.buy} <br> ${Consumer.date}> <button 
-                class="btn btn-accent btn-active" onclick="ListConsumer.editConsumer(${index})">Edit</button><button 
-                class="btn btn-accent btn-active" onclick="ListConsumer.deleteConsumer(${index})"> delete </button></div></div></h4>`;;
-                });
-        }
-        
+const dbConsumer = {
+    save(ListConsumer2) {
+        localStorage.setItem('ListConsumer', JSON.stringify(ListConsumer2));
+    },
+    get() {
+        return JSON.parse(localStorage.getItem('ListConsumer'));
     }
 }
+ const num = {
+    showNum: function () {
+        this.ListConsumer = dbConsumer.get();
+        let consum = null
+            for(consum = 0; consum <= this.ListConsumer.length; consum++) {
+                document.getElementById("consumerNum").innerHTML = consum ;
+            }
+        }
+    }
+num.showNum();

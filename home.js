ee54be7647de66f26fb7d1ac92972d008d46a84b
difasyafitri1204 <89449073+difasyafitri1204@users.noreply.github.com>
2023-03-31 11:01:22 +0700

    +=+

--- home.js
+++ home.js
@@ -15,4 +15,23 @@ const dbConsumer = {
             }
         }
     }
+
+    const dbAdd = {
+        save(ListAdd2) {
+            localStorage.setItem('ListAdd', JSON.stringify(ListAdd2));
+        },
+        get() {
+            return JSON.parse(localStorage.getItem('ListAdd'));
+        }
+    }
+     const productNum = {
+        showNumm: function () {
+            this.ListAdd = dbAdd.get();
+            let prosum = null
+                for(prosum = 0; prosum <= this.ListAdd.length; prosum++) {
+                    document.getElementById("numProduct").innerHTML = prosum ;
+                }
+            }
+        }
 num.showNum();
+productNum.showNumm();
